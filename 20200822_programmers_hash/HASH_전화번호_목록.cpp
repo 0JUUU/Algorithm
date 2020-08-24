d0d3baef7f542d15380729c35615d5b29165f757
YOUNGJU <clleo97@naver.com>
2020-08-24 18:41:25 +0900

    sort_complete

---
+++ 20200822_programmers_hash/HASH_전화번호_목록.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+        for (int j = i + 1; j < phone_book.size(); j++)
+        {
+            if (phone_book[j].find(phone_book[i]) <= phone_book.size())
+            {
+                answer = false;
+                break;
+            }
+            if (!answer) break;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<string> list = { "119", "97674223", "1195524421" };
+    cout << solution(list) << endl;
+
+    list = { "123" ,"456", "789" };
+    cout << solution(list) << endl;
+
+    list = { "12", "123", "1235", "567","88" };
+    cout << solution(list) << endl;
+}
