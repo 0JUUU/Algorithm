1dcc9704a8a6ca5547193f1f866eafef10527da4
YOUNGJU <clleo97@naver.com>
2020-08-30 23:45:37 +0900

    NOT_FINISTHED

---
+++ 20200830_programmers_level_test/lv2_H_index.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    int hindex = 0;
+    return answer;
+}
+
+int main()
+{
+    vector<int> citations = { 3, 0 ,6, 1, 5 };
+
+    // 3
+    cout << solution(citations) << endl;
+}
